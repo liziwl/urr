97e17094f08a981bc77ed12dab241cd0f3db33ee
adelinac <adelina.ionulescu@gmail.com>
2017-05-22 16:23:16 +0200

    Added dummy dropdown menu for filtering classes.

--- static/myscript.js
+++ static/myscript.js
@@ -17,3 +17,26 @@ function disableButtonState(elem) {
 $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip();
 });
+
+var options = [];
+
+$( '.dropdown-menu a' ).on( 'click', function( event ) {
+    console.log(">>>>>>>>>>> ON CLICK!!!")
+   var $target = $( event.currentTarget ),
+       val = $target.attr( 'data-value' ),
+       $inp = $target.find( 'input' ),
+       idx;
+
+   if ( ( idx = options.indexOf( val ) ) > -1 ) {
+      options.splice( idx, 1 );
+      setTimeout( function() { $inp.prop( 'checked', false ) }, 0);
+   } else {
+      options.push( val );
+      setTimeout( function() { $inp.prop( 'checked', true ) }, 0);
+   }
+
+   $( event.target ).blur();
+
+   console.log( options );
+   return false;
+});
