7734cb661f5c9c1915384610f2cc58c71e16a472
adelinac <adelina.ionulescu@gmail.com>
2017-09-12 06:26:12 +0200

    Modified data_analysis script.

--- classification/data_analysis.py
+++ classification/data_analysis.py
@@ -7,51 +7,13 @@ from __future__ import print_function
 
 import numpy as np
 import pandas as pd
+from classification.classification_utils import *
+from collections import Counter
 
 CATEGORIES = [u'IS_COMPLAINT', u'IS_PRIVACY', u'IS_HARDWARE', u'IS_DEVICE', u'IS_PERFORMANCE', u'IS_BATTERY',
               u'IS_PRICE', u'IS_APP USABILITY', u'IS_ANDROID VERSION', u'IS_UI', u'IS_LICENSING', u'IS_MEMORY',
               u'IS_SECURITY']
-
-
-NAME_PACKAGE_DICT = {
-                    'Adblock Plus': 'org.adblockplus.adblockplussbrowser',
-                    'Pixel Dungeon': 'com.watabou.pixeldungeon',
-                    'Bubble level': 'com.gamma.bubblelevel',
-                    'A Comic Viewer': 'net.androidcomics.acv',
-                    'Muzei Live Wallpaper': 'net.nurik.roman.muzei',
-                    'Signal Private Messenger': 'org.thoughtcrime.securesms',
-                    'Amaze File Manager': 'com.amaze.filemanager',
-                    'DashClock Widget': 'net.nurik.roman.dashclock',
-                    'OctoDroid': 'com.gh4a',
-                    'Device Control [root]': 'org.namelessrom.devicecontrol',
-                    'AntennaPod': 'de.danoeh.antennapod',
-                    'Xabber': 'com.xabber.android',
-                    'MultiPicture Live Wallpaper': 'org.tamanegi.wallpaper.multipicture',
-                    'Duck Duck GO': 'com.duckduckgo.mobile.android',
-                    'Wally': 'me.wally.android.plus',
-                    'QKSMS - Open Source SMS & MMS': 'com.moez.QKSMS',
-                    'SeriesGuide': 'com.battlelancer.seriesguide',
-                    'OS Monitor': 'com.eolwral.osmonitor',
-                    'Simon Tatham\'s Puzzles': 'name.boyle.chris.sgtpuzzles',
-                    'ConnectBot': 'org.connectbot',
-                    'Terminal Emulator for Android': 'jackpal.androidterm',
-                    'BatteryBot Battery Indicator': 'com.darshancomputing.BatteryIndicator',
-                    'Network Log': 'com.googlecode.networklog',
-                    'c:geo': 'cgeo.geocaching',
-                    'Abstract Art': 'net.georgewhiteside.android.abstractart',
-                    'Marine Compass': 'net.pierrox.mcompass',
-                    'Turbo Editor ( Text Editor )': 'com.maskyn.fileeditor',
-                    'Twidere for Twitter': 'org.mariotaku.twidere',
-                    'Clip Stack ✓ Clipboard Manager': 'com.catchingnow.tinyclipboardmanager',
-                    'Autostarts': 'com.elsdoerfer.android.autostarts',
-                    'Financius - Expense Manager': 'com.code44.finance',
-                    'Calculator': 'com.xlythe.calculator.material',
-                    'Last.fm': 'fm.last.android',
-                    'AcDisplay': 'com.achep.acdisplay',
-                    'AnkiDroid Flashcards': 'com.ichi2.anki',
-                    'CatLog': 'com.nolanlawson.logcat',
-                    'Tweet Lanes': 'com.tweetlanes.android',
-                    }
+APP_DATA_DIR = "./data/app_data/"
 
 
 def print_category_data(data, category):
@@ -71,17 +33,42 @@ category_words = ["donating", "donate", "donation", "$", "bucks", "free",
                   ]
 
 
+def print_data_info(filename):
+    data = pd.read_csv(filename)
+    print("For filename %s total reviews: %d" % (filename, len(data)))
+    print(data.columns)
+    for category in build_categories_list():
+        category_data = data.loc[data[category] == 1]
+        print("For category %s : %d" % (category, len(category_data)))
+
+
+def split_data(data_filepath, apps_dir):
+    data = pd.read_csv(data_filepath)
+    apps = set(data["app"])
+    for app in apps:
+        apps_data = data.loc[data["app"] == app]
+        print("For app: %s, we have %d reviews" % (app, len(apps_data)))
+        app_filename = "_".join(app.split()) + ".csv"
+        app_filepath = os.path.join(apps_dir, app_filename)
+        apps_data.to_csv(app_filepath, index=False)
+        print("Saving app data to: %s" % app_filepath)
+
+
+def print_app_infos(data_filepath):
+    data = pd.read_csv(data_filepath)
+    apps = set(data["app"])
+    apps_counter = Counter()
+    for app in apps:
+        app_data = data.loc[data["app"] == app]
+        apps_counter[app] = len(app_data)
+    print(apps_counter)
+    return apps_counter
+
+
 def main():
-    data = pd.read_csv("./data/train_reviews_manual_copy_04.csv")
-    additional_data = pd.read_csv("./data/additional_data_02.csv")
-    print("Total reviews: %d" % len(data))
-    print(len(additional_data))
-    ids = set(additional_data["_id"])
-    print(len(ids))
-    additional_data.drop_duplicates(subset=['_id'], inplace=True)
-    print(len(additional_data))
-    # complete_data.to_csv("./data/complete_data.csv", index=False)
-    print(additional_data.columns)
+    # print_data_info("./data/all_reviews_data.csv")
+    # split_data("./data/all_reviews_data.csv", "./data/app_data/")
+    print_app_infos("./data/all_reviews_data.csv")
 
 
 if __name__ == "__main__":
